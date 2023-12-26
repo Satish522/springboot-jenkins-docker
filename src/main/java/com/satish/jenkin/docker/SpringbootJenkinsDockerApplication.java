ff5fc61142ec0910c780e45c53daa14695786c71
Satish522 <satishsubudhi522@gmail.com>
2023-12-26 06:33:13 +0530

    Added api

--- src/main/java/com/satish/jenkin/docker/SpringbootJenkinsDockerApplication.java
+++ src/main/java/com/satish/jenkin/docker/SpringbootJenkinsDockerApplication.java
@@ -2,12 +2,19 @@ package com.satish.jenkin.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootJenkinsDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJenkinsDockerApplication.class, args);
 	}
-
+	
+	@GetMapping("/docker")
+	public String welcome() {
+		return "Publish to Docker";
+	}
 }
